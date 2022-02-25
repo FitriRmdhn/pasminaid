380ccf79deb9ba2a6996871e646824c96794aa94
FitriRmdhn <97644480+FitriRmdhn@users.noreply.github.com>
2022-02-25 09:28:37 +0700

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -31,7 +31,7 @@
       </section>
     </div>
     <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">© 2020 Copyright:
-      <a class="text-white" href="https://mdbootstrap.com/">pasmina.id</a>
+      <a class="text-white" href="#">pasmina.id</a>
     </div>
   </footer>
 </section>
